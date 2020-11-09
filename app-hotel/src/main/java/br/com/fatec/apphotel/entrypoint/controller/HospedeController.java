c3e88582c518da3f86fb2cbea93f4d8636f7fc27
gumota <gustavo.omota@hotmail.com>
2020-11-09 16:47:54 -0300

    Ajustes na controller

--- app-hotel/src/main/java/br/com/fatec/apphotel/entrypoint/controller/HospedeController.java
+++ app-hotel/src/main/java/br/com/fatec/apphotel/entrypoint/controller/HospedeController.java
@@ -1,5 +1,33 @@
 package br.com.fatec.apphotel.entrypoint.controller;
 
-public class HospedeController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import br.com.fatec.apphotel.entrypoint.exception.ValidaCampoException;
+import br.com.fatec.apphotel.entrypoint.model.request.HospedeEntrypointModelRequest;
+import br.com.fatec.apphotel.entrypoint.validation.HospedeValidation;
+
+@RestController
+@RequestMapping(value = HospedeController.URI)
+public class HospedeController {
+	
+	protected static final String URI = "/hospede";
+	
+	private HospedeValidation hospedeValidation;
+	
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<HospedeEntrypointModelRequest> cadastrarHospede(
+			@RequestBody(required = true) HospedeEntrypointModelRequest hospedeEntrypointModelRequest) throws JsonProcessingException, ValidaCampoException {
+				
+		hospedeValidation.validarCamposObrigatorio(hospedeEntrypointModelRequest);
+		
+		return new ResponseEntity<>(hospedeEntrypointModelRequest, HttpStatus.CREATED);	
+	}
 }
