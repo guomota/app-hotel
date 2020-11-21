e70590b3d49d0b449e92404344d5dace3918d1c0
Kim Tsunoda <kim.tsunoda@gmail.com>
2020-11-21 11:36:28 -0300

    Adicionando Produtos

---
+++ app-hotel/src/main/java/br/com/fatec/apphotel/controller/request/ProdutoEntrypointRequest.java
@@ -0,0 +1,41 @@
+package br.com.fatec.apphotel.controller.request;
+
+import br.com.fatec.apphotel.modelo.Produto;
+import br.com.fatec.apphotel.repository.ProdutoRepository;
+
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+
+public class ProdutoEntrypointRequest {
+    @NotBlank(message = "O campo descricao é obrigátorio")
+    private String descricao;
+
+    @NotBlank(message = "O campo valor é obrigátorio")
+    private BigDecimal valor;
+
+    public String getDescricao () {
+        return descricao;
+    }
+
+    public void setDescricao ( String descricao ) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor () {
+        return valor;
+    }
+
+    public void setValor ( BigDecimal valor ) {
+        this.valor = valor;
+    }
+
+    public Produto atualizar ( Long codigo , ProdutoRepository produtoRepository ) {
+        Produto produto = produtoRepository.getOne ( codigo );
+
+        produto.setDescricao ( this.descricao );
+        produto.setValor ( this.valor );
+
+        return produto;
+    }
+
+}
